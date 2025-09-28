ee568313e6076ddffbeff5ab176a2b6facc377af
saaniyahere <saaniya.ashrf@gmail.com>
2025-09-28 23:07:43 +0530

    Update 6.cpp

--- ps6/6.cpp
+++ ps6/6.cpp
@@ -16,7 +16,15 @@ class complex{
     ~complex(){
         cout<<"Complex number destroyed."<<endl;
     }
+    friend complex addComplex(const complex &c1, const complex& c2);
 };
+
+complex addComplex(const complex& c1, const complex& c2){
+    int r = c1.real+c2.real;
+    int i = c1.imag+c2.imag;
+    complex c3(r,i);
+    return c3;
+}
 
 int main(){
     complex c1(2,3);
@@ -24,6 +32,11 @@ int main(){
 
     c1.display();
     c2.display();
+
+    complex sum = addComplex(c1,c2);
+
+    cout<<"Sum = ";
+    sum.display();
 
     return 0;
 }
